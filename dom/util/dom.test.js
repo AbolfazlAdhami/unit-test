56ae81b7cdc3786e2ad03878f053f4005293fdf5
AbolfazlAdhami <aadhami880@gmail.com>
2024-11-11 19:52:27 +0330

    dom testing exp

---
+++ dom/util/dom.test.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+import { it, vi, expect, beforeEach } from "vitest";
+import { showError } from "./dom";
+
+import { Window } from "happy-dom";
+
+const htmlDocPath = path.join(process.cwd(), "index.html");
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+vi.stubGlobal("document", document);
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+  document.write(htmlDocumentContent);
+});
+
+it("should add an error message to the id='errors' element ", () => {
+  showError("test");
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
+});
+
+it("should not contain an error paragraph intially", () => {
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).toBeNull();
+});
